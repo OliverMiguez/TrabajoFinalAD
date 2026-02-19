77bf63d95533d85893f94cfaa17733573e4cbb95
XogosAquiDEV <olimial03@gmail.com>
2026-02-19 21:14:58 +0100

    Readme

--- Executor/src/main/java/org/example/Main.java
+++ Executor/src/main/java/org/example/Main.java
@@ -7,10 +7,16 @@ import org.springframework.context.annotation.ComponentScan;
 
 import java.text.ParseException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class
+})
 @ComponentScan({"org.example"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
+// http://localhost:8080/swagger-ui/index.html
